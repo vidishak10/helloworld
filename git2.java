71e99982d9c69cc6d07f12c179790ac155e49ea3
vidishak10 <105345701+vidishak10@users.noreply.github.com>
2024-01-24 23:40:41 +0530

    Update print to sum program, git2.java

--- git2.java
+++ git2.java
@@ -1,5 +1,9 @@
 public class git2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a=10;
+        int b=40;
+        int sum = a+b;
+        System.out.println("The Sum of a + b is "+sum);
+    
     }
 }
